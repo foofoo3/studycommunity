acc8ba71c16bff5a9fdd51fcaaaa46a8c69a9e75
foofoo3 <3139640799@qq.com>
2022-09-14 00:21:27 +0800

    Update PublishController.java

--- src/main/java/com/demo/community/controller/PublishController.java
+++ src/main/java/com/demo/community/controller/PublishController.java
@@ -1,15 +1,64 @@
 package com.demo.community.controller;
 
+import com.demo.community.entity.Question;
+import com.demo.community.entity.User;
+import com.demo.community.mapper.QuestionMapper;
+import com.demo.community.mapper.UserMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 @Slf4j
 @Controller
 public class PublishController {
+    @Autowired
+    private QuestionMapper questionMapper;
+    @Autowired
+    private UserMapper userMapper;
 
     @GetMapping("/publish")
     public String Publish(){
         return "publish";
+    }
+
+    @PostMapping("/publish")
+    public void DoPublish(@RequestParam("title") String title,
+                            @RequestParam("description") String description,
+                            @RequestParam("tag") String tag,
+                            HttpServletRequest request, HttpServletResponse response){
+        HttpSession session = request.getSession();
+        User user = (User) session.getAttribute("user");
+        if (user==null){
+            try {
+                response.sendRedirect("/resultLogin?=3");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }else {
+            Question question = new Question();
+            question.setTitle(title);
+            question.setDescription(description);
+            question.setTag(tag);
+            question.setCreator(user.getName());
+            question.setGmt_create(System.currentTimeMillis());
+            question.setGmt_modified(question.getGmt_create());
+
+            questionMapper.create(question);
+
+            try {
+                response.sendRedirect("/");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
