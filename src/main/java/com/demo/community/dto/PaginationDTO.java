a35556a1cbf9aeb76537a3d9d43884b0cc26ac78
foofoo3 <3139640799@qq.com>
2022-09-19 23:43:11 +0800

    Create PaginationDTO.java

---
+++ src/main/java/com/demo/community/dto/PaginationDTO.java
@@ -0,0 +1,26 @@
+package com.demo.community.dto;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class PaginationDTO {
+    private List<QuestionDTO> questions;
+    private boolean showPrevious;
+    private boolean showFirstPage;
+    private boolean showNext;
+    private boolean showEndPage;
+    private Integer page;
+    private List<Integer> pages;
+
+    public void setPagination(Integer totalCount, Integer page, Integer size) {
+        Integer totalPage = 0;
+        if (totalCount % size == 0){
+            totalPage = totalCount / size;
+        }else {
+            totalPage = totalCount / size + 1;
+        }
+        
+    }
+}
