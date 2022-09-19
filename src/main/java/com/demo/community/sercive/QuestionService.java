c909c4ec41a0fc7013864bc0b4c5f90ff4f25e72
foofoo3 <3139640799@qq.com>
2022-09-19 23:43:16 +0800

    Update QuestionService.java

--- src/main/java/com/demo/community/sercive/QuestionService.java
+++ src/main/java/com/demo/community/sercive/QuestionService.java
@@ -1,5 +1,6 @@
 package com.demo.community.sercive;
 
+import com.demo.community.dto.PaginationDTO;
 import com.demo.community.dto.QuestionDTO;
 import com.demo.community.entity.Question;
 import com.demo.community.entity.User;
@@ -21,25 +22,25 @@ public class QuestionService {
     @Autowired
     private UserMapper userMapper;
 
-    public List<QuestionDTO> list(Integer page, Integer size) {
+    public PaginationDTO list(Integer page, Integer size) {
 
         Integer offset =size *(page - 1);
 
         List<Question> questions = questionMapper.list(offset,size);
         List<QuestionDTO> questionDTOList = new ArrayList<>();
+        PaginationDTO paginationDTO = new PaginationDTO();
 
         for (Question question : questions) {
-
             User user = userMapper.SelectByUid(question.getCreator());
             QuestionDTO questionDTO = new QuestionDTO();
-
             BeanUtils.copyProperties(question,questionDTO);
             questionDTO.setUser(user);
-
             questionDTOList.add(questionDTO);
         }
-
-        return questionDTOList;
+        paginationDTO.setQuestions(questionDTOList);
+        Integer totalCount = questionMapper.count();
+        paginationDTO.setPagination(totalCount,page,size);
+        return paginationDTO;
 
     }
 }
