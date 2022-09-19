a74e4e19960c9a5cea7c17b477bce804166e91e5
foofoo3 <3139640799@qq.com>
2022-09-20 00:50:49 +0800

    Update PaginationDTO.java

--- src/main/java/com/demo/community/dto/PaginationDTO.java
+++ src/main/java/com/demo/community/dto/PaginationDTO.java
@@ -2,6 +2,7 @@ package com.demo.community.dto;
 
 import lombok.Data;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
@@ -12,7 +13,7 @@ public class PaginationDTO {
     private boolean showNext;
     private boolean showEndPage;
     private Integer page;
-    private List<Integer> pages;
+    private List<Integer> pages = new ArrayList<>();
 
     public void setPagination(Integer totalCount, Integer page, Integer size) {
         Integer totalPage = 0;
@@ -21,14 +22,36 @@ public class PaginationDTO {
         }else {
             totalPage = totalCount / size + 1;
         }
-
+//        计算页码
+        pages.add(page);
+        for (int i = 0; i <= 3; i++) {
+            if (page - i > 0){
+                pages.add(page - i,0);
+            }
+            if (page + i <= totalPage){
+                pages.add(page + i);
+            }
+        }
+        //是否显示上一页
         if (page == 1){
+            showPrevious = false;
+        }else {
+            showPrevious = true;
+        }
+        //是否显示下一页
+        if (page == totalPage){
+            showNext = false;
+        }else {
+            showNext = true;
+        }
+        //是否显示第一页
+        if (pages.contains(1)){
             showFirstPage = false;
         }else {
-            showEndPage = true;
+            showFirstPage = true;
         }
-
-        if (page == totalPage){
+        //是否显示最后页
+        if (pages.contains(totalPage)){
             showEndPage = false;
         }else {
             showEndPage = true;
