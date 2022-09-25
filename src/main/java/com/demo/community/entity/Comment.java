7d67ea08d86033d0c8282e08daa066c3ac5ca3b8
foofoo3 <3139640799@qq.com>
2022-09-25 20:32:18 +0800

    Create Comment.java

---
+++ src/main/java/com/demo/community/entity/Comment.java
@@ -0,0 +1,18 @@
+package com.demo.community.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Comment {
+    private Long id;
+    private Long parent_id;
+    private int type;
+    private int commentator;
+    private Long gmt_create;
+    private Long gmt_modified;
+    private Long like_count;
+}
