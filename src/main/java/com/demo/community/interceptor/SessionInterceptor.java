f3a318c92b1953daaaf3d87428f29981a3b7c0f3
foofoo3 <3139640799@qq.com>
2022-10-05 23:59:43 +0800

    Update SessionInterceptor.java

--- src/main/java/com/demo/community/interceptor/SessionInterceptor.java
+++ src/main/java/com/demo/community/interceptor/SessionInterceptor.java
@@ -1,25 +1,38 @@
-//package com.demo.community.interceptor;
-//
-//import org.springframework.stereotype.Service;
-//import org.springframework.web.servlet.HandlerInterceptor;
-//import org.springframework.web.servlet.ModelAndView;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//
-//@Service
-//public class SessionInterceptor implements HandlerInterceptor {
-//    @Override
-//    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-//        return HandlerInterceptor.super.preHandle(request, response, handler);
-//    }
-//
-//    @Override
-//    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-//        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
-//    }
-//
-//    @Override
-//    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-//        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
-//    }
-//}
+package com.demo.community.interceptor;
+
+import com.demo.community.entity.User;
+import com.demo.community.sercive.NotificationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@Service
+public class SessionInterceptor implements HandlerInterceptor {
+    @Autowired
+    private NotificationService notificationService;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        HttpSession session = request.getSession();
+        User user = (User) session.getAttribute("user");
+        if (user != null){
+            Long unreadCount = notificationService.unreadCount(user.getUid());
+            session.setAttribute("unreadCount",unreadCount);
+        }
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+    }
+}
