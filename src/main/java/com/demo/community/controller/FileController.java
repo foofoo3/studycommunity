16fc6500c3c36f473192b4d877b0ee142cfe75d1
foofoo3 <3139640799@qq.com>
2022-10-07 19:54:03 +0800

    Update FileController.java

--- src/main/java/com/demo/community/controller/FileController.java
+++ src/main/java/com/demo/community/controller/FileController.java
@@ -12,7 +12,7 @@ public class FileController {
     public FileDTO upload(){
         FileDTO fileDTO = new FileDTO();
         fileDTO.setSuccess(1);
-        fileDTO.setUrl("/images/123.jpg");
+        fileDTO.setUrl("");
         return fileDTO;
     }
 }
