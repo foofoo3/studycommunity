129944b47156832390d3c2dafa45e9d50b9fdfe9
foofoo3 <3139640799@qq.com>
2022-12-13 23:12:36 +0800

    Update User.java

--- src/main/java/com/demo/community/entity/User.java
+++ src/main/java/com/demo/community/entity/User.java
@@ -1,5 +1,6 @@
 package com.demo.community.entity;
 
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableLogic;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -15,6 +16,7 @@ public class User {
     private String name;
     private int number;
     private String password;
+    @TableId("uid")
     private int uid ;
     private String description;
     private String face;
